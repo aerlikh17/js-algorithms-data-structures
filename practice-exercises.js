78b961cfa6289bca75d5fc828e77c9a5781fad27
aerlikh <aerlikh2011@gmail.com>
2023-03-18 16:55:39 -0400

    anagram 2nd solution

--- practice-exercises.js
+++ practice-exercises.js
@@ -3,6 +3,7 @@
 // An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, 
 // formed from iceman.
 
+// 1st solution
 function validAnagram(str1, str2){
     
     // add whatever parameters you deem necessary - good luck!
@@ -28,3 +29,33 @@ const str2 = 'cinema';
 const isStr2AnagramOfStr1 = validAnagram(str1, str2);
 
 console.log(isStr2AnagramOfStr1);
+
+// 2nd solution using Frequency Counter Pattern. this approach is more efficient than sorting the two strings and comparing them character by character, since it only loops through each string once and uses an object to keep track of the frequency of each character.
+
+function validAnagram(str1, str2) {
+    if (str1.length !== str2.length){
+        return false;
+    }
+
+    // Create an object to store frequency of each letter in the first string and increment their frequency in the lookup object
+    const lookup = {}
+
+    // Loop through the first string
+    for (let i = 0; i < str1.length; i++) {
+        let letter = str1[i];
+        // If letter exists in the lookup object, increment, otherwise set to 1
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+
+    // Loop through the second string 
+    for (let i = 0; i < str2.length; i++) {
+        let letter = second[i];
+        // If can't find letter or letter is already zero then it is not an anagram
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+    return true; 
+}
