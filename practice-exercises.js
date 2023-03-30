e54f780b69250def9e94a883f0c81fb55b4ac690
aerlikh <aerlikh2011@gmail.com>
2023-03-30 15:03:33 -0400

    bubbleSort optimization

--- practice-exercises.js
+++ practice-exercises.js
@@ -270,15 +270,19 @@ function findLongestSubstring(str) {
   // 7. BubbleSort function
 
   function bubbleSort (arr) {
+    let noSwaps;
     for (let i = arr.length; i > 0; i--) {
+        noSwaps=true;
         for (let j = 0; j < i - 1; j++) {
             if(arr[j] > arr[j+1]){
                 // SWAP! 
                 let temp = arr[j];
                 arr[j] = arr [j+1];
                 arr[j+1] = temp;
+                noSwaps = false;
             }
         }
+        if(noSwaps) break;
     }
     return arr;
   }
