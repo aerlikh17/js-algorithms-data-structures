33f66def35ee66c508e75bde9d59608b9052c966
aerlikh <aerlikh2011@gmail.com>
2023-04-07 12:29:31 -0400

    countDown

--- practice-exercises.js
+++ practice-exercises.js
@@ -355,3 +355,15 @@ function mergeSort(arr){
     let right = mergeSort(arr.slice(mid));
     return merge(left, right);
 }
+
+// 12. Recursive function countDown
+
+function countDown(num){
+    if(num <= 0){
+        console.log("All done!");
+        return;
+    }
+    console.log(num);
+    num--;
+    countDown(num);
+}
