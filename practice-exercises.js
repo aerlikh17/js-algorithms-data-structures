5b73b02b163ec464772ae52087c3c43c66caed0f
aerlikh <aerlikh2011@gmail.com>
2023-04-07 13:32:14 -0400

    helper method

--- practice-exercises.js
+++ practice-exercises.js
@@ -391,3 +391,21 @@ function factorial(num){
     if(num === 1) return 1;
     return num * factorial(num-1);
 }
+
+// 16. Helper method recursion
+
+function collectOddValues(arr){
+    let result = [];
+
+    function helper(helperInput){
+        if(helperInput.length === 0){
+            return;
+        }
+        if(helperInput[0] % 2 !== 0){
+            result.push(helperInput[0])
+        }
+        helper(helperInput.slice(1))
+    }
+    helper(arr)
+    return result;
+}
