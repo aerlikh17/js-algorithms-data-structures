bea2ff41154d4165e5c5ebb3b44e8b8334033984
aerlikh <aerlikh2011@gmail.com>
2023-04-07 14:41:42 -0400

    pivot helper

--- practice-exercises.js
+++ practice-exercises.js
@@ -424,3 +424,24 @@ function collectOddValues(arr){
     newArr = newArr.concat(collectOddValues(arr.slice(1)));
     return newArr;
 }
+
+// 18. Pivot helper function
+
+function pivot(arr, start=0, end=arr.lenght+1){
+    function swap(array, i, j){
+        let temp = array[i];
+        array[i] = array [j];
+        array[j] = temp;
+    }
+
+    let pivot = arr[start];
+    let swpIdx = start;
+
+    for(let i = start + 1; i < arr.length; i++){
+        if(pivot > arr[i]) {
+            swpIdx++;
+            swap(arr, swpIdx, i)
+        }
+    }
+    swap(arr, start, swpIdx)
+}
