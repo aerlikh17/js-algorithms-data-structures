6a006c96db79d0c3bb4ed617060238ace5558f4c
aerlikh <aerlikh2011@gmail.com>
2023-04-10 20:07:24 -0400

    digitCount

--- practice-exercises.js
+++ practice-exercises.js
@@ -468,3 +468,8 @@ function radixSort(){
 function getDigit(num, i){
     return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
+
+function digitCount(num){
+    if (num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
