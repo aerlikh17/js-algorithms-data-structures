045377c024bbdf457428cb11583cdc56a0ad03bd
aerlikh <aerlikh2011@gmail.com>
2023-04-11 13:13:09 -0400

    data structures

---
+++ data-structures.js
@@ -0,0 +1,10 @@
+// The method to create new objects must be called constructor.
+// The class keyword creates a constant, so you cannot redefine it. 
+
+class Student {
+    constructor(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
