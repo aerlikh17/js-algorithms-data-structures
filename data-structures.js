f928228359f3e38ad6b97bf7e48b33519b058f4e
aerlikh <aerlikh2011@gmail.com>
2023-04-17 15:09:41 -0400

    push()

--- data-structures.js
+++ data-structures.js
@@ -164,6 +164,19 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+    push(val){
+        let newNode = new Node(val);
+        if(this.length === 0){
+            this.tail = newNode;
+            this.head = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this; 
+    }
 }
 
 
